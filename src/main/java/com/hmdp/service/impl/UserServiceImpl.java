ab28ebac284f0d024f47b6dd2418442c8a04f8b4
hbh <2510109840@qq.com>
2026-07-17 19:19:35 +0800

    使用reddis替代session

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -64,8 +64,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         }
         // 3.符合，生成验证码
         String code = RandomUtil.randomNumbers(6);
-        // 4.保存验证码到session
-        session.setAttribute(LOGIN_CODE_KEY + phone, code);
+        // 4.ToDo
+        // 4.1 保存验证码到redis中
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY + phone, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
         // 5.发送验证码，这里简单打印日志，后续完善去发送短信验证码
         log.debug("发送验证码到手机号：{}，验证码：{}", phone, code);
         // 返回ok
@@ -84,11 +85,12 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         // 1.校验手机号
         String phone = loginForm.getPhone();
         if(RegexUtils.isPhoneInvalid(phone)){
-            return Result.fail("手机号不一致！");
+            return Result.fail("手机号格式错误！");
         }
         // 2.校验验证码
         String code = loginForm.getCode();
-        String cacheCode = session.getAttribute(LOGIN_CODE_KEY + phone).toString();
+        String cacheCode = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY + phone);
+        //String cacheCode = session.getAttribute(LOGIN_CODE_KEY + phone).toString();
         if(!cacheCode.equals(code)||code==null) {
             return Result.fail("验证码不一致！");
         }
@@ -99,10 +101,25 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
              user = createUserWithPhone(phone);
         }
         //保存到session中
+        //ToDo
         UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);//将user对象转换为userDTO对象,只复制id、nickName、icon字段
-        session.setAttribute("user", userDTO);
-        return Result.ok();
+        //session.setAttribute("user", userDTO);
+        //随机生成token
+        String token = UUID.randomUUID().toString();
+        // 将用户信息保存到redis中
+        HashMap<String,String> hashMap = new HashMap<>();
+        hashMap.put("id",user.getId().toString());
+        hashMap.put("nickName",user.getNickName());
+        hashMap.put("icon",user.getIcon());
+        stringRedisTemplate.opsForHash().putAll(LOGIN_USER_KEY + token, hashMap);
+        // 设置过期时间，这样设置后，用户登录后，token过期时间就是5分钟，用户在5分钟内没有操作，就会自动过期所以需要重新登录
+        stringRedisTemplate.expire(LOGIN_USER_KEY + token, LOGIN_USER_TTL, TimeUnit.MINUTES);
+        // 6.返回token
+        return Result.ok(token);
+
+
     }
+
 
     private User createUserWithPhone(String phone) {
         User user = new User();
