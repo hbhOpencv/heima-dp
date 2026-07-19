64bd82fe8f5c539c6b5bd064eae64934b0d64769
hbh <2510109840@qq.com>
2026-07-19 14:01:16 +0800

    缓存null值来解决缓存穿透

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -16,8 +16,7 @@ import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -36,15 +35,19 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         //redis查询商铺缓存
         String s = stringRedisTemplate.opsForValue().get(CACHE_SHOP_KEY + id);
         //判断缓冲是否命中
-        if(StrUtil.isBlank(s)) {
+        if(StrUtil.isBlank(s)) {//缓存为空
             Shop shop = getById(id);
             //数据库中查询商铺是否存在
             //不存在
-            if(shop == null)  return Result.fail("商铺不存在");
+            if(shop == null)  {
+                stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+                return Result.fail("商铺不存在");
+            }
             //存在则保存到redis中
             stringRedisTemplate.opsForValue().set(CACHE_SHOP_KEY + id, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
             return Result.ok(shop);
         }
+        if("".equals(s)) return Result.fail("商铺不存在");//缓存中为空字符串
         //返回商铺信息
         Shop shop = JSONUtil.toBean(s, Shop.class);
         return Result.ok(shop);
